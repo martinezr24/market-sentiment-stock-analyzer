d87f0286535b23ba8bde21227bc940d9fde0d3ad
Gio Martinez <giovannimart22@gmail.com>
2025-09-24 01:14:37 -0400

    Created file to calculate the sentiments of haedlines

---
+++ src/sentiment.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+
+
+nltk.download('vader_lexicon')
+
+def compute_sentiment(news_df):
+    # initialize the sentiment analyzer
+    sia = SentimentIntensityAnalyzer()
+
+    # making sure that all the headlines are strings and applying the sentiment analysis to all the headlines
+    news_df['headline'] = news_df['headline'].astype(str)
+    news_df['compound'] = news_df['headline'].apply(lambda t: sia.polarity_scores(t)['compound'])
+    # get the average of all headlines that day and return the dataframe
+    daily_sent = news_df.groupby('date_only')['compound'].mean().reset_index()
+    daily_sent = daily_sent.rename(columns={'date_only': 'Date', 'compound':'sentiment'})
+    daily_sent['Date'] = pd.to_datetime(daily_sent['Date'])
+    return daily_sent
