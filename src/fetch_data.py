2e43e7a4d4c92b14a30ec7591d6cd98e7782373c
Gio Martinez <giovannimart22@gmail.com>
2025-09-24 00:55:56 -0400

    Fixed a few bugs with reading in the data and made the date columns normalized

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas as pd
 
-def get_stock_data(ticker="AAPL", start="2024-01-01", end="2024-12-01"):
+def get_stock_data(ticker="AAPL", start="2024-01-01", end="2024-12-31"):
     # get the stock data
     df = yf.download(ticker, start=start, end=end)
 
@@ -13,13 +13,18 @@ def get_stock_data(ticker="AAPL", start="2024-01-01", end="2024-12-01"):
     df['return'] = df['Close'].pct_change()
     df = df.dropna()
     
-    #print (df)
+    # correctly indexes the entries and makes dates normalized
     df = df.reset_index()          
-    #print (df)
-    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = pd.to_datetime(df['Date']).dt.normalize()
     
+    # returns a table of stock data over a period of time
     return df
 
 def load_headlines(csv_path="data/apple_news_data.csv"):
+    # read in the csv file and normalize the date column
     news = pd.read_csv(csv_path, parse_dates=['date'])
+    news['date'] = pd.to_datetime(news['date']).dt.tz_localize(None).dt.normalize()
+
+    # only return a date and a headline column
+    news = news[['date', 'headline']]
     return news[['date', 'headline']]
