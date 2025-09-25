bf9c83cdeeed7ac3704510432d66d19009bd95ee
Gio Martinez <giovannimart22@gmail.com>
2025-09-25 01:35:31 -0400

    Further cleaned up the sentiment file

--- src/sentiment.py
+++ src/sentiment.py
@@ -5,11 +5,12 @@ import nltk
 nltk.download('vader_lexicon')
 
 def compute_sentiment(news_df):
-    # initialize the sentiment analyzer
+    '''
+    takes in a DataFrame of headlines and dates and applies a sentiment analysis to each entry.
+    '''
     sia = SentimentIntensityAnalyzer()
-    print (sia.polarity_scores("OpenAI Adds Search Engine to ChatGPT, Challenging Google"))
 
-    # making sure that all the headlines are strings and applying the sentiment analysis to them
+    # makes sure each headline is a string and then applies the sentiment analysis to each one
     news_df['Headline'] = news_df['Headline'].astype(str)
     news_df['compound'] = news_df['Headline'].apply(lambda t: sia.polarity_scores(t)['compound'])
 
